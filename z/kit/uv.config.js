db0ab171da44329d3b2088c398c5ad9a6c34a08a
VIN07grinder <vihaankrao@gmail.com>
2025-10-04 16:13:13 -0500

    Update uv.config.js

--- z/kit/uv.config.js
+++ z/kit/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/z/-kit/',
-    bare:'https://vbois-bare-server.vercel.app/bare/',
+    bare:'https://vbois-bare-server.vercel.app',
     handler: '/z/kit/uv.handler.js',
     bundle: '/z/kit/uv.bundle.js',
     config: '/z/kit/uv.config.js',
